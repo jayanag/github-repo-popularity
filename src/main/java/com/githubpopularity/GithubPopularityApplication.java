e90cce7b987ef6768f5e463177acce9c4803dc64
Dhananjayan <jayan.psg@gmail.com>
2025-10-02 11:35:27 +0200

    Initial Spring Boot setup for GitHub Popularity project

---
+++ src/main/java/com/githubpopularity/GithubPopularityApplication.java
@@ -0,0 +1,13 @@
+package com.githubpopularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPopularityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPopularityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubpopularity/GithubPopularityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubpopularity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPopularityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
